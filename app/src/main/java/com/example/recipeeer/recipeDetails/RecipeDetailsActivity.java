8b554206330d4e9a8470fa08e91518625fbfb308
Unknown <michalekmato@gmail.com>
2019-04-15 18:04:43 +0200

    Added back button for RecipeDetails activity

--- app/src/main/java/com/example/recipeeer/recipeDetails/RecipeDetailsActivity.java
+++ app/src/main/java/com/example/recipeeer/recipeDetails/RecipeDetailsActivity.java
@@ -1,8 +1,10 @@
 package com.example.recipeeer.recipeDetails;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.recipeeer.R;
 
@@ -12,5 +14,20 @@ public class RecipeDetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_details);
+
+        Toolbar mToolbar = findViewById(R.id.mToolbar);
+        setSupportActionBar(mToolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("New recipe");
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
